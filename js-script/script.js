0a270f191d8680c8c51a91424cd9a3ad489ab949
Egor <ruraegor@gmail.com>
2019-04-19 12:13:27 +0300

    fix template

--- js-script/script.js
+++ js-script/script.js
@@ -2,12 +2,9 @@
 
 //----- VARRIAL GLOBAL ----
 
-$('.bl-city__item').on('click', function () {
-
-	// $('.bl-city__item').removeClass('city__item');
+$('.js-bl-slide__item').on('click', function () {
 
 	var thisEl = $(this);
-
 
     // let clip = '20px';
     // let hor3 = '80%';
@@ -67,6 +64,8 @@ function clipRend(hor3 = '50%', vert3='50%', clip = '80px', ii = 2) {
     $('.clip_' + ii).css({'clip-path': clipFun});
 }
 
+
+//====  RANDOM COORDINAT
 function perioClip() {
 
     for (let y = 2; y < 6; y++) {
@@ -81,22 +80,28 @@ function perioClip() {
 }
 
 
-$('#header').click(function () {
-    perioClip();
-});
-
 
 //=====  time change
 setInterval(()=>{
-	perioClip();
+	// perioClip();
 }, 5000);
 
 
 
+//===========
+// $('.characters-modal-descr__charact-descr').slimscroll({
+// 	height: '220px',
+// 	alwaysVisible: true,
+// 	color: 'yellow',
+// 	size: '3px',
+// 	opacity: 0.8
+// });
+
+
 $(function(){
-    setTimeout('moveSize()', 2000);
-    setTimeout('moveWind()', 3500);
-    setTimeout('chTrans()', 15000);
+    setTimeout(moveSize, 2000);
+    setTimeout(moveWind, 3500);
+    setTimeout(chTrans, 15000);
 });
 
 function moveWind() {
@@ -109,103 +114,5 @@ function moveSize() {
 
 function chTrans() {
     $('#main-bl').removeClass('begTrans');
-}
-
-//-------------------------
-
-$(function () {
-
-
-	$(".obj-svg-path-style").on("click", function () {
-
-		var $self = $(this);
-
-		if ($self.hasClass("active")) {
-
-			$(".sect-charact__bg").removeClass("active");
-			$(".sect-charact .char path").removeClass("active");
-
-		} else {
-
-			$(".sect-charact__bg").addClass("active");
-			$self.addClass("active");
-		}
-
-	});
-
-
-	$(".sect-charact__bg").on("click", function () {
-
-		var $self = $(this);
-
-		if ($self.hasClass("active")) {
-
-			$(".sect-charact__bg").removeClass("active");
-			$(".sect-charact .char path").removeClass("active");
-		}
-
-		closeModalCharact();
-	});
-
-	//----------------
-
-	$(".js-close-modal-char").on("click", function () {
-
-		$(".sect-charact__bg").removeClass("active");
-		$(".sect-charact .char path").removeClass("active");
-
-		closeModalCharact();
-	});
-
-
-	//==== MODAL-WINDOW-CHARACTER
-	//=======================================================================================================
-
-	$(".js-characters-modal").on("click", function(){
-
-		if( $(".characters-modal-descr").hasClass("active") ) {
-
-			$(".characters-modal-descr").fadeOut(600, function(){
-
-				$(this).removeClass("active");
-			});
-
-		} else {
-
-			var chPosit = $(this).closest('div').position();
-
-			var chWidthEl = $(this).closest('div').outerWidth();
-
-
-			$(".characters-modal-descr").css({
-				"top": chPosit.top - 330,
-				"left": chWidthEl/2 + chPosit.left,
-			});
-
-
-			$(".characters-modal-descr").delay(800).fadeIn(600, function(){
-
-				$(this).addClass("active");
-			});
-		}
-
-		return false;
-
-	});
-
-	
-});
-
-
-// FUNCTIONS
-// ===========================================
-
-
-
-function closeModalCharact() {
-
-	$(".characters-modal-descr").fadeOut(600, function(){
-		$(this).removeClass("active");
-	});
 }
 
